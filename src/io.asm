f8689e034265d2015f1ed37f6ee9cc6dbef2435e
chaos086 <luisa.marinh@uqvirtual.edu.co>
2024-10-23 11:02:38 -0500

    Se prueba la conexió con el git

--- src/io.asm
+++ src/io.asm
@@ -1,30 +1,30 @@
 section .text
     global leer_numero, leer_operacion, mostrar_resultado
 
-leer_numero:
     ; Leer número desde la entrada
+leer_numero:
     push mensaje_num
     call printf
-    add esp, 4          ; Limpiar la pila
+    add esp, 4          ; Limpiar
     push numero
-    call scanf          ; Leer el número del usuario
-    add esp, 4          ; Limpiar la pila
+    call scanf          ; Leer el número
+    add esp, 4          ; Limpiar
     ret
 
-leer_operacion:
     ; Leer la operación (+, -, *, /, %)
+leer_operacion:
     push mensaje_op
     call printf
-    add esp, 4          ; Limpiar la pila
+    add esp, 4          ; Limpiar
     push operacion
-    call scanf          ; Leer la operación del usuario
-    add esp, 4          ; Limpiar la pila
+    call scanf          ; Leer la operación
+    add esp, 4          ; Limpiar
     ret
 
-mostrar_resultado:
     ; Mostrar el resultado en consola
-    push eax            ; Guardar el resultado en la pila
-    push resultado      ; Mensaje del formato de resultado
+mostrar_resultado:
+    push eax            ; Guardar el resultado
+    push resultado      ; Mensaje del formato
     call printf
-    add esp, 8          ; Limpiar la pila
+    add esp, 8          ; Limpiar
     ret
